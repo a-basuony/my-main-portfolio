fff243058426af4c004ebd3779dcd4c063999f52
Ahmed Basuony <A.Basuony.social@gmail.com>
2024-05-04 22:54:32 +0300

    "Add lazy loading image, emailjs for form submission, and update package lock and json files; update Footer.jsx to include lazy loading image, emailjs for form submission, and form handling with useRef and useEmailjs hooks;

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
+import emailjs from "@emailjs/browser";
 
 import { images } from "../../constants";
 import { AppWrap, MotionWrap } from "../../wrapper";
@@ -14,51 +15,46 @@ const Footer = () => {
   const [isFormSubmitted, setIsFormSubmitted] = useState(false);
   const [loading, setLoading] = useState(false);
   const { name, email, message } = formData;
+  const form = useRef();
 
-  function handleChangeInput(e) {
+  const handleChangeInput = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
-  }
+  };
 
-  function handleSubmit() {
+  const sendEmail = (e) => {
+    e.preventDefault();
     setLoading(true);
 
-    const contact = {
-      _type: "contact",
-      name: formData.username,
-      email: formData.email,
-      message: formData.message,
-    };
-
-    client
-      .create(contact)
-      .then(() => {
-        setLoading(false);
-        setIsFormSubmitted(true);
+    emailjs
+      .sendForm("service_3m0b2wg", "template_sfr87um", form.current, {
+        publicKey: "_8MdLhn9_yF0p8_zC",
       })
-      .catch((err) => console.log(err));
-  }
+      .then(
+        () => {
+          setLoading(false);
+          setIsFormSubmitted(true);
+          console.log("SUCCESS!");
+        },
+        (error) => {
+          console.log("FAILED...", error.text);
+        }
+      );
+  };
 
-  return (
-    <>
-      <h2 className="head-text">Take a coffee & chat with me</h2>
-
-      <div className="app__footer-cards">
-        <div className="app__footer-card">
-          <img src={images.email} alt="email" />
-          <a href="mailto:basuoney.chrom@gmail.com" className="p-text">
-            basuoney.chrom@gmail.com
-          </a>
+  const renderForm = () => {
+    if (isFormSubmitted) {
+      return (
+        <div>
+          <h3 className="head-text">Thank you for getting in touch!</h3>
         </div>
-        <div className="app__footer-card">
-          <img src={images.mobile} alt="mobile" />
-          <a href="tel:+201125240491" className="p-text">
-            01125240491
-          </a>
-        </div>
-      </div>
-
-      {!isFormSubmitted ? (
-        <div className="app__footer-form app__flex">
+      );
+    } else {
+      return (
+        <form
+          className="app__footer-form app__flex"
+          ref={form}
+          onSubmit={sendEmail}
+        >
           <div className="app__flex">
             <input
               type="text"
@@ -88,15 +84,34 @@ const Footer = () => {
               onChange={handleChangeInput}
             />
           </div>
-          <button type="button" className="p-text" onClick={handleSubmit}>
+          <button type="submit" className="p-text">
             {loading ? "Sending..." : "Send Message"}
           </button>
+        </form>
+      );
+    }
+  };
+
+  return (
+    <>
+      <h2 className="head-text">Take a coffee & chat with me</h2>
+
+      <div className="app__footer-cards">
+        <div className="app__footer-card">
+          <img src={images.email} alt="email" />
+          <a href="mailto:basuoney.chrom@gmail.com" className="p-text">
+            basuoney.chrom@gmail.com
+          </a>
         </div>
-      ) : (
-        <div>
-          <h3 className="head-text">Thank you for getting in touch!</h3>
+        <div className="app__footer-card">
+          <img src={images.mobile} alt="mobile" />
+          <a href="tel:+201125240491" className="p-text">
+            01125240491
+          </a>
         </div>
-      )}
+      </div>
+
+      {renderForm()}
     </>
   );
 };
--- src/container/Skills/Skills.jsx
+++ src/container/Skills/Skills.jsx
@@ -27,11 +27,11 @@ const Skills = () => {
       <h2 className="head-text">Skills & Experience</h2>
       <div className="app__skills-container">
         <motion.div className="app__skills-list">
-          {skills.map((skill) => (
+          {skills.map((skill, index) => (
             <motion.div
               whileInView={{ opacity: [0, 1] }}
               transition={{ duration: 0.5 }}
-              key={skill.name}
+              key={skill.name + index}
               className="app__skills-item app__flex"
             >
               <div
@@ -46,13 +46,16 @@ const Skills = () => {
         </motion.div>
 
         <div className="app__skills-exp">
-          {experiences.map((experience) => (
-            <motion.div className="app__skills-exp-item" key={experience.year}>
+          {experiences.map((experience, index) => (
+            <motion.div
+              className="app__skills-exp-item"
+              key={experience.year + index}
+            >
               <div className="app__skills-exp-year">
                 <p className="bold-text">{experience.year}</p>
               </div>
               <motion.div className="app__skills-exp-works">
-                {experience.works.map((work) => (
+                {experience.works.map((work, index) => (
                   <>
                     <motion.div
                       whileInView={{ opacity: [0, 1] }}
@@ -60,7 +63,7 @@ const Skills = () => {
                       className="app__skills-exp-work"
                       data-tip
                       data-for={work.name}
-                      key={work.name}
+                      key={work.name + index}
                     >
                       <h4 className="bold-text">{work.name}</h4>
                       <p className="p-text">{work.company}</p>
